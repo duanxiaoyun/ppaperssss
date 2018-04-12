628b828e7fbe22667d3f4e0fafe48b5fe0133369
duanxiaoyun <cloudgaga750@gmail.com>
2018-04-12 19:56:25 +0900

    1231
    45

---
+++ bi_ye_lun_wen/cao gao/0408/ChemVRNew/Assets/_Test/GameSystem/Scripts/Meitiao.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meitiao : MonoBehaviour {
+
+    public bool isFire = false;
+    public bool isFired { get; private set; }
+    public GameObject fire;
+    public MeshRenderer offsetControll;
+
+
+	// Use this for initialization
+	void Start () {
+        isFired = false;
+        SetFire(isFire);
+    }
+	
+	// Update is called once per frame.
+	void Update () {
+        if (isFire)
+        {
+            float deltaTime = Time.deltaTime * 0.33f;
+            offsetControll.material.mainTextureOffset += new Vector2(0.5f, 0) * deltaTime;
+            fire.transform.localPosition += fire.transform.right * deltaTime;
+            if (fire.transform.localPosition.x >= 0.5f) {
+                SetFire(false);
+                isFired = true;
+            }
+        }
+	}
+
+    public void SetFire(bool active) {
+        isFire = active;
+        fire.SetActive(active);
+    }
+
+
+}
---
+++ bi_ye_lun_wen/cao gao/0408/ChemVRNew/Assets/_Test/GameSystem/Scripts/SceneManager2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManager2 : MonoBehaviour {
+
+    public Image middlepic;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void loadangfa()
+    {
+        middlepic.GetComponent<Image>().sprite = Resources.Load<Sprite>("fangfa");
+    }
+    public void loadyuansubiao()
+    {
+        middlepic.GetComponent<Image>().sprite = Resources.Load<Sprite>("ShinyPeriodicTable_1");
+    }
+    public void loadyuanli()
+    {
+        middlepic.GetComponent<Image>().sprite = Resources.Load<Sprite>("yuanli");
+    }
+    public void loadintro()
+    {
+        middlepic.GetComponent<Image>().sprite = Resources.Load<Sprite>("intro");
+    }
+    public void loadvideo()
+    {
+        middlepic.GetComponent<Image>().sprite = Resources.Load<Sprite>("video");
+    }
+
+    public void GoToMain()
+    {
+        SceneManager.LoadScene("Main");
+        Debug.Log("Main");
+    }
+}
