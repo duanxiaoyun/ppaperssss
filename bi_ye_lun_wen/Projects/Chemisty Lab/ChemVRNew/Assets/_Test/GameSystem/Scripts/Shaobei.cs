31de09e1652b1e848e05e6ac11086a03d6a49537
duanxiaoyun <cloudgaga750@gmail.com>
2018-05-22 22:51:44 +0900

    05223
    05223

---
+++ bi_ye_lun_wen/Projects/Chemisty Lab/ChemVRNew/Assets/_Test/GameSystem/Scripts/Shaobei.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaobei : MonoBehaviour {
+
+    public GameObject qipao;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "meitiao1"&& other.GetComponent<Meitiao>().isFired)
+        {
+            qipao.SetActive(true);
+        }
+    }
+}
